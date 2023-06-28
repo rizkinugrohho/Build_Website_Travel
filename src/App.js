dfa69ff38d5bd23d3299dc106b88dd6461ef5077
Rizki Nugroho <rizkynhg@gmail.com>
2023-06-29 03:22:26 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import 'assets/scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Hai dunia!
+    </div>
+  );
+}
+
+export default App;
